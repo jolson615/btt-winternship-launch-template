cd8a698edb62679576effbf2d3e90f98f169df52
Glitch (hello-webpage) <none>
2020-11-24 17:01:07 +0000

    👌💝 Checkpoint ./sketch2.js:574715/1374 ./sketch2.html:574715/105 ./sketch1.js:574715/10

--- sketch2.js
+++ sketch2.js
@@ -1,15 +1,29 @@
 /* 
-global createCanvas, stroke, noFill, windowWidth, windowHeight, colorMode, HSL, textSize, text, random, background, fill, ellipse, mouseX, mouseY, collideCircleCircle, width, height, color, abs, noStroke, sqrt, PI
+global createCanvas, stroke, triangle, noFill, windowWidth, windowHeight, colorMode, HSL, textSize, text, random, background, fill, ellipse, mouseX, mouseY, collideCircleCircle, width, height, color, abs, noStroke, sqrt, PI
 */
 // List all p5 methods you plan to use in the global so that Glitch can recognize them.
 
 
 function setup() {
   createCanvas(600, 500);
+  noStroke();
 }
 
 function draw() {
   background(220)
+ 
   
+  fill(80,0,0)
+  rect()
+  fill(80,0,0)
+  rect()
+  fill()
+  rect()
+  fill()
+  rect()
+  fill()
+  rect()
+  fill()
+  rect()
 }
 

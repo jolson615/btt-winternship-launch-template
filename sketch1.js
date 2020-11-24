b04aa151eab41732f375e3ba0da17a9647a6f668
Glitch (hello-webpage) <none>
2020-11-24 17:12:16 +0000

    😎🏬 Checkpoint ./sketch2.js:574715/769 ./sketch1.js:574715/6

--- sketch1.js
+++ sketch1.js
@@ -1,5 +1,5 @@
 /* 
-global createCanvas, stroke, triangle, noFill, windowWidth, windowHeight, colorMode, HSL, textSize, text, random, background, fill, ellipse, mouseX, mouseY, collideCircleCircle, width, height, color, abs, noStroke, sqrt, PI
+global createCanvas, stroke, triangle, rect, noFill, windowWidth, windowHeight, colorMode, HSL, textSize, text, random, background, fill, ellipse, mouseX, mouseY, collideCircleCircle, width, height, color, abs, noStroke, sqrt, PI
 */
 // List all p5 methods you plan to use in the global so that Glitch can recognize them.
 
--- sketch2.js
+++ sketch2.js
@@ -1,5 +1,5 @@
 /* 
-global createCanvas, stroke, triangle, noFill, windowWidth, windowHeight, colorMode, HSL, textSize, text, random, background, fill, ellipse, mouseX, mouseY, collideCircleCircle, width, height, color, abs, noStroke, sqrt, PI
+global createCanvas, stroke, triangle, rect, noFill, windowWidth, windowHeight, colorMode, HSL, textSize, text, random, background, fill, ellipse, mouseX, mouseY, collideCircleCircle, width, height, color, abs, noStroke, sqrt, PI
 */
 // List all p5 methods you plan to use in the global so that Glitch can recognize them.
 
@@ -13,17 +13,11 @@ function draw() {
   background(220)
  
   
-  fill(80,0,0)
-  rect()
-  fill(80,0,0)
-  rect()
-  fill()
-  rect()
-  fill()
-  rect()
-  fill()
-  rect()
-  fill()
-  rect()
+  fill(200,0,0)
+  rect(150, 200, 300, 300)
+  triangle(150, 200, 450, 200, 300, 50)
+  
+  fill(220)
+  rect(250, 350, 100, 150)
 }
 

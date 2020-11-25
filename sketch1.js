48753a0dffd5394762efa4442ceb0f4911c8873c
Glitch (hello-webpage) <none>
2020-11-25 19:26:28 +0000

    👠🏍 Checkpoint ./sketch2.js:574715/3243 ./sketch1.js:574715/1694

--- sketch1.js
+++ sketch1.js
@@ -1,5 +1,5 @@
 /* 
-global createCanvas, stroke, triangle, rect, noFill, windowWidth, windowHeight, colorMode, HSL, textSize, text, random, background, fill, ellipse, mouseX, mouseY, collideCircleCircle, width, height, color, abs, noStroke, sqrt, PI
+global createCanvas, round, stroke, triangle, rect, noFill, windowWidth, windowHeight, colorMode, HSL, textSize, text, random, background, fill, ellipse, mouseX, mouseY, collideCircleCircle, width, height, color, abs, noStroke, sqrt, PI
 */
 // List all p5 methods you plan to use in the global so that Glitch can recognize them.
 
@@ -11,6 +11,9 @@ function setup() {
 
 function draw() {
   background(220); // Paints the canvas a nice, neutral grey
+  // Shows mouse position, just to be helpful.
+  fill(0, 0, 0) // Change color to black for text
+  text(`x: ${round(mouseX)}, y: ${round(mouseY)}`, 10, 10) 
   
   // Blue-ish circle code
   fill(0, 190, 240)
--- sketch2.js
+++ sketch2.js
@@ -1,23 +1,26 @@
 /* 
-global createCanvas, stroke, triangle, rect, noFill, windowWidth, windowHeight, colorMode, HSL, textSize, text, random, background, fill, ellipse, mouseX, mouseY, collideCircleCircle, width, height, color, abs, noStroke, sqrt, PI
+global createCanvas, round, stroke, triangle, rect, noFill, windowWidth, windowHeight, colorMode, HSL, textSize, text, random, background, fill, ellipse, mouseX, mouseY, collideCircleCircle, width, height, color, abs, noStroke, sqrt, PI
 */
 // List all p5 methods you plan to use in the global so that Glitch can recognize them.
 
 
 function setup() {
-  createCanvas(600, 500);
-  noStroke();
+  createCanvas(600, 500); // Sizes the canvas, making width = 600 and height = 500
+  noStroke(); // Setting to draw our shapes without outlines
 }
 
 function draw() {
-  background(220)
- 
+  background(220); // Paints the canvas a nice, neutral grey
+  // Shows mouse position, just to be helpful.
+  fill(0, 0, 0) // Change color to black for text
+  text(`x: ${round(mouseX)}, y: ${round(mouseY)}`, 10, 10) 
   
-  fill(200,0,0)
-  rect(150, 200, 300, 300)
-  triangle(150, 200, 450, 200, 300, 50)
+  // Blue-ish circle code
+  fill(0, 190, 240)
+  ellipse(50, 50, 50, 50)
   
-  fill(220)
-  rect(250, 350, 100, 150)
+  // Purplish circle code
+  fill(90, 0, 222)
+  ellipse(550, 50, 50)
 }
 
